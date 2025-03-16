e1d9eca0a06b593a5bd1946e398ee8bdec2f19ee
anassbentaib <banasstaib@gmail.com>
2025-03-16 21:12:52 +0000

    Use full path for gunicorn

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,5 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 
-# Ensure gunicorn is run from the installed directory
-export PATH="$HOME/.local/bin:$PATH"
-gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application
+# Use full path for gunicorn
+/Library/Frameworks/Python.framework/Versions/3.13/bin/gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application
