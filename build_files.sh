779177ed01dbfd6ae1118f4331dfb99272511f0a
anassbentaib <banasstaib@gmail.com>
2025-03-16 21:24:46 +0000

    ser-tracking.wsgi:application

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,11 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 
-# Add Python 3.13 bin directory to PATH
-export PATH="/Library/Frameworks/Python.framework/Versions/3.13/bin:$PATH"
+# Add Python bin directory to PATH dynamically
+export PATH="$(python3 -m site --user-base)/bin:$PATH"
+
+# Debug: Check if gunicorn is accessible
+echo "Gunicorn Path: $(which gunicorn)"
 
 # Start Gunicorn
 gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application
