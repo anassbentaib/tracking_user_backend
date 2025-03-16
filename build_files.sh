dff9569b6527f37f19d4da79c29ff37c50e66255
anassbentaib <banasstaib@gmail.com>
2025-03-16 21:13:06 +0000

    Start Gunicorn

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,8 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 
-# Use full path for gunicorn
-/Library/Frameworks/Python.framework/Versions/3.13/bin/gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application
+# Add Python 3.13 bin directory to PATH
+export PATH="/Library/Frameworks/Python.framework/Versions/3.13/bin:$PATH"
+
+# Start Gunicorn
+gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application
