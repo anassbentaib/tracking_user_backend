18e656bdd8fa4ee7b6379a7e008757a38ce15b8c
anassbentaib <banasstaib@gmail.com>
2025-03-16 21:09:36 +0000

    gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application

--- build_files.sh
+++ build_files.sh
@@ -2,3 +2,7 @@
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
+
+# Ensure gunicorn is run from the installed directory
+export PATH="$HOME/.local/bin:$PATH"
+gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application
