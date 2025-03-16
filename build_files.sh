e514a832147f9571d20795d38be74eb292d71fb5
anassbentaib <banasstaib@gmail.com>
2025-03-16 21:26:23 +0000

    manage.py collectstati

--- build_files.sh
+++ build_files.sh
@@ -3,11 +3,5 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput
 
-# Add Python bin directory to PATH dynamically
-export PATH="$(python3 -m site --user-base)/bin:$PATH"
-
-# Debug: Check if gunicorn is accessible
-echo "Gunicorn Path: $(which gunicorn)"
-
-# Start Gunicorn
-gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application
+# Use full path to gunicorn
+/python312/bin/gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi:application
