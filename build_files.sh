549566101f8dfa95ada0e2003c77a0759a509680
anassbentaib <banasstaib@gmail.com>
2025-03-16 20:53:58 +0000

    update build file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e  # Exit immediately if any command fails
+
+echo "Installing dependencies..."
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+echo "Running collectstatic..."
 python3 manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+gunicorn --bind 0.0.0.0:8000 user-tracking.wsgi
