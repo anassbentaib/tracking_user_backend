6f566aa0d7ae976564a6477ca10796c099571162
anassbentaib <banasstaib@gmail.com>
2025-03-17 09:28:21 +0000

    Create and activate a virtual environment

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# Build the project
-echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+# Create and activate a virtual environment
+echo "Setting up virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+echo "Installing dependencies..."
+pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
