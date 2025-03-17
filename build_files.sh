1948bb3b9edc6c87644c01c48395e502d64b5668
anassbentaib <banasstaib@gmail.com>
2025-03-17 09:35:27 +0000

    venv/bin/activate

--- build_files.sh
+++ build_files.sh
@@ -5,6 +5,9 @@ echo "Setting up virtual environment..."
 python3 -m venv venv
 source venv/bin/activate
 
+# Upgrade pip to avoid dependency issues
+pip install --upgrade pip
+
 # Install dependencies
 echo "Installing dependencies..."
 pip install -r requirements.txt
